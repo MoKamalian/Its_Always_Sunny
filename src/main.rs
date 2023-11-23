00dd97c2a2bd46f12bb564def2cd5f5b19940f49
Mo Kamalian <am445669@dal.ca>
2023-11-23 12:53:44 -0400

    Formatting printing to console

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,114 @@
 /**
-
 @author: ammir kamalian
 @date: 28 sep 2023
-
 */
 
+mod utility;
 
-mod Utility;
+use reqwest;
+use std::error::Error;
+use utility::WeatherResponse;
+use crate::utility::IWeather;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+
+  /** @brief API functionality. This function makes the request to OpenWeatherMap
+  to fetch weather data. */
+  let http_response = reqwest::get("https://api.openweathermap.org/data/2.5/weather?q=halifax&appid=")
+  .await?;
+  let json_response = http_response.json::<WeatherResponse>().await?;
+
+  //lambda for converting between K to C
+  let closure_convertT: fn(&f64) -> f64 = |i: &f64| -> f64 { i - 273.15 } ;
+
+  // weather information
+  let city_name: &String = json_response.getCityName();
+  let description: &String = json_response.getMainDescription();
+  let main: &String = json_response.getMain();
+  let temp: f64 = closure_convertT(json_response.getTemp());
+  let feels_like: f64 = closure_convertT(json_response.getFeelsLike());
+  let pressure: &f64 = json_response.getPressure();
+  let humidity: &f64 = json_response.getHumidity();
 
 
-fn main() {
+  // for formatting for txt being printed
+  let width: usize = 8;
 
-    println!("Hello, world!");
 
+  if(main == "Thunderstorm") {
+   // print thunderstorm art
+   utility::weather::Thunderstorm();
+   println!("\nCity:{city:>width$}\n
+             Temperature:{temp:>width$}\n
+             Feels Like:{fl:>width$}\n
+             Pressure:{pressure:>width$}\n
+             Humidity:{humidity:>width$}",
+            city=city_name,
+            temp=temp,
+            fl=feels_like,
+            pressure=pressure,
+            humidity=humidity);
+
+  } else if(main == "Drizzle") {
+    // print drizzle art
+   utility::weather::Drizzle();
+
+  } else if(main == "Rain") {
+    // print rain art
+   utility::weather::Rain();
+
+  } else if(main == "Snow") {
+    // print snow art
+   utility::weather::Snow();
+
+  } else if(main == "Fog") {
+    // print fog art
+   utility::weather::Fog();
+
+  } else if(main == "Mist") {
+    // print mist art
+   utility::weather::Mist();
+   println!("\nCity: {city:>width$}\n
+             Temperature: {temp:>width$}\n
+             Feels Like: {fl:>width$}\n
+             Pressure: {pressure:>width$}\n
+             Humidity: {humidity:>width$}",
+            city=city_name,
+            temp=temp,
+            fl=feels_like,
+            pressure=pressure,
+            humidity=humidity);
+
+  } else if(main == "Clear") {
+    // print clear art
+   utility::weather::Clear();
+
+  } else if(main == "Clouds") {
+    // print clouds art
+   utility::weather::Clouds();
+   println!("\nCity:{city:>width$}\n
+             Temperature:{temp:>width$}\n
+             Feels Like:{fl:>width$}\n
+             Pressure:{pressure:>width$}\n
+             Humidity:{humidity:>width$}",
+            city=city_name,
+            temp=temp,
+            fl=feels_like,
+            pressure=pressure,
+            humidity=humidity);
+
+  } else {
+    // all other cases print nothing
+
+  }
+
+  Ok(())
 }
+
+
+
+
 
 
 
--- src/utility.rs
+++ src/utility.rs
@@ -3,51 +3,191 @@
 @date: 28 sep 2023
  */
 
-mod Utility {
+use serde::Deserialize;
+
+pub mod weather {
 
     // UI ACII art style function calls
-    pub trait WeatherASCIIArt {
-        fn cloudy(&self);
-        fn raining(&self);
-        fn drizzle(&self);
-        fn sunny(&self);
-        fn partially_sunny(&self);
-        fn snowing(&self);
-        fn snow_storm(&self);
+    pub fn Thunderstorm() -> &'static str {
+        let ASCII_thunderstorm: &str = r#"
+                   ___(                        )
+                   (                          _)
+                  (_                       __))
+                    ((                _____)
+                      (_________)----'
+                         _/  /
+                        /  _/
+                      _/  /
+                     / __/
+                   _/ /
+                  /__/
+                 //
+                /'
+        "#;
+        return ASCII_thunderstorm;
     }
 
-    struct City {
-        lat: i64,
-        lon: i64,
-        weather: EnvData
+    pub fn Drizzle() -> &'static str {
+        let ASCII_drizzile: &str = r#"
+                          (`  ).                   _
+                         (     ).              .:(`  )`.
+                       _(       '`.          :(   .    )
+                    .=(`(      .   )     .--  `.  (    ) )
+                   ((    (..__.:'-'   .+(   )   ` _`  ) )
+            `.     `(       ) )       (   .  )     (   )  ._
+              )      ` __.:'   )     (   (   ))     `-'.-(`  )
+            )  )  ( )       --'       `- __.'         :(      ))
+            .-'  (_.'          .')                    `(    )  ))
+                              (_  )                     ` __.:'
+
+
+        "#;
+        return ASCII_drizzile;
+    }
+    pub fn Rain() {
+
+    }
+    pub fn Snow() {
+
+    }
+    pub fn Fog() {
+
     }
 
-    struct EnvData {
-        temp: i64,
-        feels_like: i64,
-        humidity: i64,
-        pressure: i64,
-        description: String
+    pub fn Mist() {
+
     }
 
-    impl WeatherASCIIArt for City {
-        fn cloudy(&self) {}
-        fn raining(&self) {}
-        fn drizzle(&self) {}
-        fn sunny(&self) {}
-        fn partially_sunny(&self) {}
-        fn snowing(&self) {}
-        fn snow_storm(&self) {}
+    pub fn Clear() {
+
     }
 
-    /** API functions */
+    pub fn Clouds() {
 
-    // api call function
-
-    // parser
-
-
+    }
 
 
 }
+
+/** @brief basic functionality to retrieve weather information. */
+pub trait IWeather {
+    fn getMain(&self) -> &String;
+    fn getMainDescription(&self) -> &String;
+    fn getTemp(&self) -> &f64;
+    fn getFeelsLike(&self) -> &f64;
+    fn getPressure(&self) -> &f64;
+    fn getHumidity(&self) -> &f64;
+    fn getTimeZone(&self) -> &f64;
+    fn getCityName(&self) -> &String;
+
+}
+
+/** @brief json response container */
+#[derive(Deserialize, Debug)]
+pub struct WeatherResponse {
+    coord: Coord,
+    weather: Weather,
+    base: String,
+    main: Main,
+    visibility: f64,
+    wind: Wind,
+    clouds: Clouds,
+    dt: f64,
+    sys: Sys,
+    timezone: f64,
+    id: f64,
+    name: String,
+    cod: f64
+}
+
+/** function implementations for WeatherResponse. These are
+ mostly getters although additional functionality may be added.*/
+impl IWeather for WeatherResponse {
+    /** he main descriptor that is used to describe the weather */
+    fn getMain(&self) -> &String {
+        return &self.weather.data.main;
+    }
+
+    fn getMainDescription(&self) -> &String {
+        return &self.weather.data.description;
+    }
+
+    fn getTemp(&self) -> &f64 {
+        return &self.main.temp;
+    }
+
+    fn getFeelsLike(&self) -> &f64 {
+        return &self.main.feels_like;
+    }
+
+    fn getPressure(&self) -> &f64 {
+        return &self.main.pressure;
+    }
+
+    fn getHumidity(&self) -> &f64 {
+        return &self.main.humidity;
+    }
+
+    fn getTimeZone(&self) -> &f64 {
+        return &self.timezone;
+    }
+
+    fn getCityName(&self) -> &String {
+        return &self.name;
+    }
+
+}
+
+
+#[derive(Deserialize, Debug)]
+pub struct Coord {
+    lon: f64,
+    lat: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Weather {
+    data: WData,
+}
+
+#[derive(Deserialize, Debug)]
+pub struct WData {
+    id: f64,
+    main: String,
+    description: String,
+    icon: String
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Main {
+    temp: f64,
+    feels_like: f64,
+    temp_min: f64,
+    temp_max: f64,
+    pressure: f64,
+    humidity: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Wind {
+    speed: f64,
+    deg: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Clouds {
+    all: f64
+}
+
+#[derive(Deserialize, Debug)]
+pub struct Sys {
+    r#type: f64,
+    id: f64,
+    country : String,
+    sunrise: f64,
+    sunset: f64
+}
+
+
+
 
