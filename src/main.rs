2d244ca2c0a6369f61679023c11d432764271ee0
Mo Kamalian <am445669@dal.ca>
2023-12-04 11:01:06 -0400

    all ascii art added

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@
 
 mod utility;
 
+use std::io;
 use reqwest;
 use std::error::Error;
 use utility::WeatherResponse;
@@ -12,98 +13,140 @@ use crate::utility::IWeather;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-
-  /** @brief API functionality. This function makes the request to OpenWeatherMap
-  to fetch weather data. */
-  let http_response = reqwest::get("https://api.openweathermap.org/data/2.5/weather?q=halifax&appid=")
-  .await?;
-  let json_response = http_response.json::<WeatherResponse>().await?;
-
-  //lambda for converting between K to C
-  let closure_convertT: fn(&f64) -> f64 = |i: &f64| -> f64 { i - 273.15 } ;
-
-  // weather information
-  let city_name: &String = json_response.getCityName();
-  let description: &String = json_response.getMainDescription();
-  let main: &String = json_response.getMain();
-  let temp: f64 = closure_convertT(json_response.getTemp());
-  let feels_like: f64 = closure_convertT(json_response.getFeelsLike());
-  let pressure: &f64 = json_response.getPressure();
-  let humidity: &f64 = json_response.getHumidity();
+    /** user city input and validation */
+    println!("Please input the name of the City: ");
+    let mut input_city = String::new();
+    io::stdin().read_line(&mut input_city);
 
 
-  // for formatting for txt being printed
-  let width: usize = 8;
+    /** @brief API functionality. This function makes the request to OpenWeatherMap
+    to fetch weather data. */
+    let mut api_string: String = String::new();
 
 
-  if(main == "Thunderstorm") {
-   // print thunderstorm art
-   utility::weather::Thunderstorm();
-   println!("\nCity:{city:>width$}\n
-             Temperature:{temp:>width$}\n
-             Feels Like:{fl:>width$}\n
-             Pressure:{pressure:>width$}\n
-             Humidity:{humidity:>width$}",
-            city=city_name,
-            temp=temp,
-            fl=feels_like,
-            pressure=pressure,
-            humidity=humidity);
+    let http_response = reqwest::get("https://api.openweathermap.org/data/2.5/weather?q=halifax&appid=")
+    .await?;
+    let json_response = http_response.json::<WeatherResponse>().await?;
 
-  } else if(main == "Drizzle") {
+    //lambda for converting between K to C
+    let closure_convertT: fn(&f64) -> f64 = |i: &f64| -> f64 { i - 273.15 } ;
+
+    // weather information
+    let city_name: &String = json_response.getCityName();
+    let description: &String = json_response.getMainDescription();
+    let main: &String = json_response.getMain();
+    let temp: f64 = closure_convertT(json_response.getTemp());
+    let feels_like: f64 = closure_convertT(json_response.getFeelsLike());
+    let pressure: &f64 = json_response.getPressure();
+    let humidity: &f64 = json_response.getHumidity();
+
+
+    // for formatting for txt being printed
+    let width: usize = 8;
+
+
+    if(main == "Thunderstorm") {
+    // print thunderstorm art
+    println!("{art}", art=utility::weather::Thunderstorm());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
+
+    } else if(main == "Drizzle") {
     // print drizzle art
-   utility::weather::Drizzle();
+    println!("{art}", art=utility::weather::Drizzle());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Rain") {
+    } else if(main == "Rain") {
     // print rain art
-   utility::weather::Rain();
+    println!("{art}", art=utility::weather::Rain());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Snow") {
+    } else if(main == "Snow") {
     // print snow art
-   utility::weather::Snow();
+    println!("{art}", art=utility::weather::Snow());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Fog") {
+
+    } else if(main == "Fog") {
     // print fog art
-   utility::weather::Fog();
+    println!("{art}", art=utility::weather::Fog());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Mist") {
+    } else if(main == "Mist") {
     // print mist art
-   utility::weather::Mist();
-   println!("\nCity: {city:>width$}\n
-             Temperature: {temp:>width$}\n
-             Feels Like: {fl:>width$}\n
-             Pressure: {pressure:>width$}\n
-             Humidity: {humidity:>width$}",
-            city=city_name,
-            temp=temp,
-            fl=feels_like,
-            pressure=pressure,
-            humidity=humidity);
+    println!("{art}", art=utility::weather::Mist());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Clear") {
+    } else if(main == "Clear") {
     // print clear art
-   utility::weather::Clear();
+    println!("{art}", art=utility::weather::Clear());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else if(main == "Clouds") {
+    } else if(main == "Clouds") {
     // print clouds art
-   utility::weather::Clouds();
-   println!("\nCity:{city:>width$}\n
-             Temperature:{temp:>width$}\n
-             Feels Like:{fl:>width$}\n
-             Pressure:{pressure:>width$}\n
-             Humidity:{humidity:>width$}",
-            city=city_name,
-            temp=temp,
-            fl=feels_like,
-            pressure=pressure,
-            humidity=humidity);
+    println!("{art}", art=utility::weather::Clouds());
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  } else {
-    // all other cases print nothing
+    } else {
+    // all other cases print no ascii art, simply output the  weather information
+    println!("Todays Weather: {main}",main=main);
+    println!("City:{city:>width$}\nTemperature: {temp:>width$}\nFeels Like: {fl:>width$.2}\nPressure: {pressure:>width$}\nHumidity: {humidity:>width$}",
+             city=city_name,
+             temp=temp,
+             fl=feels_like,
+             pressure=pressure,
+             humidity=humidity);
 
-  }
+    }
 
-  Ok(())
+    Ok(())
 }
 
 
